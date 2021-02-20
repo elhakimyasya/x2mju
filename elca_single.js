d5a58141ea1af2e48393816e23275a04dd664d51
Yasya El Hakim <yasyaelhakim@gmail.com>
2021-02-20 23:53:23 +0700

    Update elca_single.js

--- elca_single.js
+++ elca_single.js
@@ -1,7 +1,7 @@
 function adBlockNotDetected() {}
 
 var postBody = document.getElementById('post-body'),
-var adBlockContent = '<div id="post-body"><blockquote><strong style="display: flex;align-items: center;"><svg width="24" height="24" viewBox="0 0 24 24" style="fill:currentColor;margin-right: 10px;"><path d="M11,15H13V17H11V15M11,7H13V13H11V7M12,2C6.47,2 2,6.5 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20Z"></path></svg>Ad-Block Terdeteksi</strong><p>Mohon maaf, kami mendeteksi bahwa anda telah mengaktifkan Ad-Blocker. Untuk itu, kami berharap agar anda menonaktifkannya.</p><p>Dengan menonaktifkan Ad-Blocker, anda telah membantu kami dalam mengembangkan Website ini. Terimakasih</p></blockquote></div>';
+adBlockContent = '<div id="post-body"><blockquote><strong style="display: flex;align-items: center;"><svg width="24" height="24" viewBox="0 0 24 24" style="fill:currentColor;margin-right: 10px;"><path d="M11,15H13V17H11V15M11,7H13V13H11V7M12,2C6.47,2 2,6.5 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20Z"></path></svg>Ad-Block Terdeteksi</strong><p>Mohon maaf, kami mendeteksi bahwa anda telah mengaktifkan Ad-Blocker. Untuk itu, kami berharap agar anda menonaktifkannya.</p><p>Dengan menonaktifkan Ad-Blocker, anda telah membantu kami dalam mengembangkan Website ini. Terimakasih</p></blockquote></div>';
 
 function adBlockDetected() {
 postBody.innerHTML = adBlockContent;
