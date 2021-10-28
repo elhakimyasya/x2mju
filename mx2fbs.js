37a971c84cd882cef96b3418d99a191929c4e1e0
Yasya El Hakim <yasyaelhakim@gmail.com>
2021-10-28 11:55:37 +0700

    Update mx2fbs.js

--- mx2fbs.js
+++ mx2fbs.js
@@ -41,10 +41,10 @@ function elcreativeAuthProfile () {
         var postContent;
         postItem.forEach(function($s) {
           entry = $s.val();
-          postContent = '<div class="auth_article"><div class="article_info"><a href="' + authUserPostPage + '?id=' + $s.getKey() + '" title="' + entry.title + '">' + entry.title + '</a><small>' + datetimeFormat(entry.updated) + '</small></div><div class="article_action">Pending | Edit | Delete</div></div>';
+          postContent = '<div class="auth_article"><div class="article_info"><a href="' + authUserPostPage + '?id=' + $s.getKey() + '" title="' + entry.title + '">' + entry.title + '</a><small>' + datetimeFormat(entry.updated) + '</small></div><div class="article_action">Pending | Edit | Delete</div></div>' + postContent;
         });
 
-        document.querySelector(".auth_post_container").classList.remove("loading"), document.querySelector(".tab_panel_post").innerHTML = postContent;
+        document.querySelector(".auth_post_container").classList.remove("loading"), document.querySelector(".tab_panel_post").appendChild(postContent);
       });
     } else {
       window.location.href = authLoginPage;
