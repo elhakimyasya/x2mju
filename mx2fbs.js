cfbbb8dabf1283ae9df4926046abe5fd047ef31e
Yasya El Hakim <yasyaelhakim@gmail.com>
2021-10-29 16:36:33 +0700

    Update mx2fbs.js

--- mx2fbs.js
+++ mx2fbs.js
@@ -123,66 +123,66 @@ function elcreativeAuthPostEdit() {
           document.getElementById("auth_input_post_labels").value = postItem.labels;
           document.getElementById("auth_input_post_content").value = postItem.content;
 
-          // tinymce.init({
-          //   selector : "textarea",
-          //   height : 500,
-          //   branding : false,
-          //   menubar : "file edit view insert format tools table",
-          //   plugins : "link image preview toc codesample table wordcount code lists insertdatetime emoticons visualblocks",
-          //   toolbar : "formatselect | bold italic underline strikethrough superscript subscript blockquote | link image |  alignleft aligncenter alignright alignjustify bullist numlist | table toc | codesample preview insertdatetime emoticons visualblocks code",
-          //   toc_class : "elcTOC",
-          //   toc_depth : 6,
-          //   content_style : 'body { font-family: "Segoe UI"}',
-          //   codesample_languages : [{
-          //     text : "Command Line",
-          //     value : "command hljs hl hljs"
-          //   }, {
-          //     text : "CSS",
-          //     value : "css hljs hl css"
-          //   }, {
-          //     text : "C",
-          //     value : "c hljs hl c"
-          //   }, {
-          //     text : "C++",
-          //     value : "cpp hljs hl cpp"
-          //   }, {
-          //     text : "HTML/XML",
-          //     value : "html hljs hl html xml"
-          //   }, {
-          //     text : "Java",
-          //     value : "java hljs hl java"
-          //   }, {
-          //     text : "JavaScript",
-          //     value : "javascript hljs hl javascript"
-          //   }, {
-          //     text : "JSON",
-          //     value : "json hljs hl json"
-          //   }, {
-          //     text : "Markdown",
-          //     value : "markdown hljs hl markdown"
-          //   }, {
-          //     text : "PHP",
-          //     value : "php hljs hl php"
-          //   }, {
-          //     text : "Python",
-          //     value : "python hljs hl python"
-          //   }, {
-          //     text : "TypeScript",
-          //     value : "typescript hljs hl typescript"
-          //   }],
-          //   insertdatetime_formats : ["Updated: %A, %d %B %Y"],
-          //   rel_list : [{
-          //     title : "Internal Link",
-          //     value : ""
-          //   }, {
-          //     title : "External Link",
-          //     value : "noopener noreferer nofollow"
-          //   }],
-          //   extended_valid_elements : "img[src|loading=lazy|alt|title|width|height|align|onmouseover|onmouseout|name]",
-          //   init_instance_callback : function(ed) {
-          //     $(".__loader").remove();
-          //   }
-          // });
+          tinymce.init({
+            selector : "textarea",
+            height : 500,
+            branding : false,
+            menubar : "file edit view insert format tools table",
+            plugins : "link image preview toc codesample table wordcount code lists insertdatetime emoticons visualblocks",
+            toolbar : "formatselect | bold italic underline strikethrough superscript subscript blockquote | link image |  alignleft aligncenter alignright alignjustify bullist numlist | table toc | codesample preview insertdatetime emoticons visualblocks code",
+            toc_class : "elcTOC",
+            toc_depth : 6,
+            content_style : 'body { font-family: "Segoe UI"}',
+            codesample_languages : [{
+              text : "Command Line",
+              value : "command hljs hl hljs"
+            }, {
+              text : "CSS",
+              value : "css hljs hl css"
+            }, {
+              text : "C",
+              value : "c hljs hl c"
+            }, {
+              text : "C++",
+              value : "cpp hljs hl cpp"
+            }, {
+              text : "HTML/XML",
+              value : "html hljs hl html xml"
+            }, {
+              text : "Java",
+              value : "java hljs hl java"
+            }, {
+              text : "JavaScript",
+              value : "javascript hljs hl javascript"
+            }, {
+              text : "JSON",
+              value : "json hljs hl json"
+            }, {
+              text : "Markdown",
+              value : "markdown hljs hl markdown"
+            }, {
+              text : "PHP",
+              value : "php hljs hl php"
+            }, {
+              text : "Python",
+              value : "python hljs hl python"
+            }, {
+              text : "TypeScript",
+              value : "typescript hljs hl typescript"
+            }],
+            insertdatetime_formats : ["Updated: %A, %d %B %Y"],
+            rel_list : [{
+              title : "Internal Link",
+              value : ""
+            }, {
+              title : "External Link",
+              value : "noopener noreferer nofollow"
+            }],
+            extended_valid_elements : "img[src|loading=lazy|alt|title|width|height|align|onmouseover|onmouseout|name]",
+            init_instance_callback : function() {
+              
+            }
+          });
         });
         // $("#update_entry").submit(function(key) {
         //   key.preventDefault();
