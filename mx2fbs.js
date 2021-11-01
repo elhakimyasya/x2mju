6015d351ec60d7914ce42540b78e4fcbd9ccef24
Yasya El Hakim <yasyaelhakim@gmail.com>
2021-11-01 18:10:22 +0700

    Update mx2fbs.js

--- mx2fbs.js
+++ mx2fbs.js
@@ -49,6 +49,8 @@ function elcreativeAuthProfile() {
         if (postContent !== "") {
           document.querySelector(".elcreative_tab .tab_button_container").innerHTML += "<button id='tab_button_post' class='tab_button elcreative_ripple' type='button' aria-label='Posts' data-toggle-target='#tab_panel_post' aria-controls='tab_panel_post' role='tab' aria-selected='true' data-toggle-radio-group='tab_auth' data-toggle-arrows='' data-toggle-class='' data-toggle-is-active=''>Your Posts</button>";
           document.querySelector(".elcreative_tab").innerHTML += "<div id='tab_panel_post' class='tab_panel_content tab_panel_post' role='tabpanel' aria-labelledby='tab_button_post' aria-hidden='true'>" + postContent + "</div>";
+
+          easyToggleState();
         }
       });
     } else {
