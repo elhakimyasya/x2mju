a279566140dc9cee232ac9688455e436b65ef47a
Yasya El Hakim <yasyaelhakim@gmail.com>
2021-11-01 16:30:54 +0700

    Update mx2fbs.js

--- mx2fbs.js
+++ mx2fbs.js
@@ -28,18 +28,14 @@ function elcreativeAuthProfile() {
       document.querySelector(".auth_profile_container").innerHTML = '<div class="auth_profile"><div class="auth_avatar"><span class="lazyload shimmer" data-image="' + database.photoURL + '"/></div><div class="auth_info"><div class="auth_name">' + database.displayName + '</div><div class="auth_email">' + database.email + '</div><div class="auth_action"><a href="/p/' + authCreatePost + '">Create Posts</a></div></div></div>';
 
       var refUser = firebase.database().ref().child('Users/' + database.uid);
-
-      var userDataS = {
+      refUser.update({
         userData : {
           userEmail: database.email,
           userName: database.displayName,
           userPhotoUrl: database.photoURL,
           userUID: database.uid
         }
-      };
-
-      refUser.push(userDataS);
-
+      });
 
 
       var refUserPost = firebase.database().ref().child('Users/' + database.uid).child("userPost");
